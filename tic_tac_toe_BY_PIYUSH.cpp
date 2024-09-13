ae405abdd11894f3bcb0bd790b6eb4a78ef6b377
Piyush Raj <132749621+Legend-Raj@users.noreply.github.com>
2024-09-14 01:29:11 +0530

    Added Computer Feature
    Added Play with computer feature

--- tic_tac_toe_BY_PIYUSH.cpp
+++ tic_tac_toe_BY_PIYUSH.cpp
@@ -35,13 +35,31 @@ bool checkWinner(char board[], char player) {
     return false;
 }
 
+int getComputerMove(char board[]) {
+    vector<int> availableMoves;
+    for (int i = 0; i < 9; i++) {
+        if (board[i] != 'X' && board[i] != 'O') {
+            availableMoves.push_back(i);
+        }
+    }
+    if (!availableMoves.empty()) {
+        srand(time(0));
+        return availableMoves[rand() % availableMoves.size()];
+    }
+    return -1;
+}
+
 int main() {
     cout << "\033[1;36m";
     cout << "Welcome to Tic Tac Toe!" << endl;
     cout << "=======================" << endl;
-    cout<<"Made By Piyush Raj  Enjoy :-)"<<endl;
+    cout << "Made By Piyush Raj. Enjoy :-)" << endl;
     cout << "\033[0m"; 
-    
+
+    char playMode;
+    cout << "Do you want to play against another player (P) or the computer (C)? ";
+    cin >> playMode;
+
     do {
         char board[9] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
         int moves = 0;
@@ -55,12 +73,28 @@ int main() {
             printBoard(board);
 
             int move;
-            cout << "\033[1;34m";
-            cout << "Player " << player << ", enter your move (1-9): ";
-            cin >> move;
-            cout << "\033[0m";
 
-            if (cin.fail()|| move < 1 || move > 9 || board[move-1] == 'X' || board[move-1] == 'O') {
+            if (playMode == 'C' || playMode == 'c') {
+                if (player == 'X') {
+                    cout << "\033[1;34m";
+                    cout << "Player " << player << ", enter your move (1-9): ";
+                    cin >> move;
+                    cout << "\033[0m";
+                } else {
+                    cout << "\033[1;34m";
+                    cout << "Computer's turn (O):" << endl;
+                    cout << "\033[0m";
+                    move = getComputerMove(board) + 1;  // Adding 1 to get the human-friendly index
+                    cout << "Computer chooses: " << move << endl;
+                }
+            } else {
+                cout << "\033[1;34m";
+                cout << "Player " << player << ", enter your move (1-9): ";
+                cin >> move;
+                cout << "\033[0m";
+            }
+
+            if (cin.fail() || move < 1 || move > 9 || board[move-1] == 'X' || board[move-1] == 'O') {
                 cin.clear();
                 cin.ignore(numeric_limits<streamsize>::max(), '\n');
                 cout << "\033[1;31m";
@@ -68,42 +102,38 @@ int main() {
                 cout << "\033[0m";
                 continue;
             }
+
             board[move-1] = player;
             moves++;
 
             winner = checkWinner(board, player);
             player = (player == 'X') ? 'O' : 'X';
         }
+
         if (winner) {
-            int x;
-            char winner_player = player == 'X' ? 'O' : 'X'; 
+            char winner_player = (player == 'X') ? 'O' : 'X'; 
             cout << "\033[1;33m";
             cout << "Player " << winner_player << " wins!" << endl;
             cout << "\033[0m";
-            }
-             else {
-                cout << "\033[1;33m";
-                cout << "It's a tie!" << endl;
-                cout << "\033[0m";
-            }   
-        
-    char playAgain;
-    cout << "\033[1;36m";
-    cout << "Thanks for playing Tic Tac Toe!" << endl;
-    cout << "\033[0m";
-    cout << "\033[1;35m";
-    cout << "Do you want to play again? (Y/N): ";
-    cin >> playAgain;
-    cout << "\033[0m";
+        } else {
+            cout << "\033[1;33m";
+            cout << "It's a tie!" << endl;
+            cout << "\033[0m";
+        }   
 
-    if (playAgain != 'Y' && playAgain != 'y') {
-        
-        break;
-    }
-} while (true);
+        char playAgain;
+        cout << "\033[1;36m";
+        cout << "Thanks for playing Tic Tac Toe!" << endl;
+        cout << "\033[0m";
+        cout << "\033[1;35m";
+        cout << "Do you want to play again? (Y/N): ";
+        cin >> playAgain;
+        cout << "\033[0m";
 
-return 0;
+        if (playAgain != 'Y' && playAgain != 'y') {
+            break;
+        }
+    } while (true);
+
+    return 0;
 }
-
-        
-       
